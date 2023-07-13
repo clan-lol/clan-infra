9204ad948d6bfd66bc91522e9d72a5fa154d972e
Jörg Thalheim <joerg@thalheim.io>
2023-07-13 18:37:14 +0200

    switch from netlify to hetznerdns

--- targets/admins/tf.sh
+++ targets/admins/tf.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 rm -f .terraform.lock.hcl
 if grep -q .sops terraform.tfstate; then
   sops -i -d terraform.tfstate
-  if [[ -f secrets.auto.tfvars.json ]]; then
-    sops -d secrets.auto.tfvars.json > secrets.auto.tfvars
-    exit 1
+  if [[ -f secrets.auto.tfvars.sops.json ]]; then
+    sops -d secrets.auto.tfvars.sops.json > secrets.auto.tfvars.json
   fi
 fi
 cleanup() {
+  rm -f secrets.auto.tfvars.json
   sops -i -e terraform.tfstate
-  rm -f secrets.auto.tfvars
 }
 
 trap "cleanup" EXIT
--- targets/web01/terraform.tf
+++ targets/web01/terraform.tf
@@ -2,12 +2,15 @@ terraform {
   backend "local" {}
 }
 
+variable "hetznerdns_token" {}
+
 module "web01" {
   source           = "../../terraform/web01"
   domain           = "clan.lol"
   netlify_dns_zone = "clan.lol"
   nixos_flake_attr = "web01"
   nixos_vars_file  = "${path.module}/nixos-vars.json"
+  hetznerdns_token = var.hetznerdns_token
   tags = {
     Terraform = "true"
     Target    = "web01"
