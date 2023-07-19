23e20f192b397528326d1b345ccbd5a90da7c966
DavHau <hsngrmpf+github@gmail.com>
2023-07-19 20:33:53 +0200

    homepage: allow all admins to deploy via www user

--- modules/web01/homepage.nix
+++ modules/web01/homepage.nix
@@ -1,13 +1,15 @@
-{ pkgs, self, ... }: {
+{ config, pkgs, self, ... }: {
   security.acme.defaults.email = "admins@clan.lol";
   security.acme.acceptTerms = true;
 
   # www user to push website artifacts via ssh
   users.users.www = {
-    openssh.authorizedKeys.keys = [
-      # ssh-homepage-key
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxZ3Av30M6Sh6NU1mnCskB16bYtNP8vskc/+ud0AU1C ssh-homepage-key"
-    ];
+    openssh.authorizedKeys.keys =
+      config.users.users.root.openssh.authorizedKeys.keys
+      ++ [
+        # ssh-homepage-key
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMxZ3Av30M6Sh6NU1mnCskB16bYtNP8vskc/+ud0AU1C ssh-homepage-key"
+      ];
     isNormalUser = true;
   };
 
