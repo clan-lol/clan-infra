4a0b6cf7850ec9e54bcb6fe2f6750fc054a898c2
Jörg Thalheim <joerg@thalheim.io>
2023-07-19 16:37:41 +0200

    found a better workaround to avoid crashes on web01

--- modules/hetzner-ex101.nix
+++ modules/hetzner-ex101.nix
@@ -9,24 +9,5 @@
   # We are not limited by zfs, so we can use the latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # looks like the Intel i9-13900 draws too much power and crashes the system
-  systemd.services.limit-cpu-freq = {
-    description = "Limit CPU frequency to 4GHz";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "systemd-modules-load.service" ];
-    # Some cores do have a scaling max freq less than 5GHz, so we need to
-    # check for that or else all cores will run at 800MHz
-    script = ''
-      #!/bin/sh
-      for f in /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq; do
-        old_val="$(<"$f")"
-        if [[ "$old_val" -gt 4000000 ]]; then
-          echo 4000000 > "$f"
-        fi
-      done
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-    };
-  };
+  powerManagement.cpuFreqGovernor = "powersave";
 }
