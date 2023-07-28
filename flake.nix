f61c3f047902f2717f6195b6e3be93051a9c2002
DavHau <hsngrmpf+github@gmail.com>
2023-07-28 15:54:51 +0200

    flake: remove darwin support

--- flake.nix
+++ flake.nix
@@ -36,8 +36,6 @@
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
       imports = [
         inputs.treefmt-nix.flakeModule
