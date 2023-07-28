e17ac46a6c52c9cc72a1334edcc468e2e76605f0
DavHau <hsngrmpf+github@gmail.com>
2023-07-28 16:56:21 +0200

    jobs: generate systemd services fro jobs automatically

--- flake-parts/action-flake-update-pr-clan/script.sh
+++ flake-parts/action-flake-update-pr-clan/script.sh
@@ -2,12 +2,11 @@
 set -euo pipefail
 
 # prevent these variables from being unset by writePureShellScript
-export KEEP_VARS="GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL GITEA_URL GITEA_USER PR_TITLE REMOTE_BRANCH REPO REPO_DIR${KEEP_VARS:+ $KEEP_VARS}"
+export KEEP_VARS="GIT_AUTHOR_NAME GIT_AUTHOR_EMAIL GIT_COMMITTER_NAME GIT_COMMITTER_EMAIL GITEA_URL GITEA_USER PR_TITLE REMOTE_BRANCH REPO_DIR${KEEP_VARS:+ $KEEP_VARS}"
 
 # configure variables for actions
 export PR_TITLE="Automatic flake update - $(date --iso-8601=minutes)"
 export REMOTE_BRANCH="flake-update-$(date --iso-8601)"
-export REPO="gitea@git.clan.lol:clan/clan-infra.git"
 export REPO_DIR=$TMPDIR/repo
 export GIT_AUTHOR_NAME="Clan Merge Bot"
 export GIT_AUTHOR_EMAIL="clan-bot@git.clan.lol"
