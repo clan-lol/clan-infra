0256d36588ebe379cc9ed00e710b8da75120e63f
Jörg Thalheim <joerg@thalheim.io>
2023-11-29 13:53:31 +0100

    fix server networkd initrd

--- modules/initrd-networking.nix
+++ modules/initrd-networking.nix
@@ -33,6 +33,7 @@ in
   config = {
     # Hack so that network is considered up by boot.initrd.network and postCommands gets executed.
     boot.kernelParams = [ "ip=127.0.0.1:::::lo:none" ];
+    boot.initrd.systemd.enable = false;
 
     boot.initrd.network = {
       enable = true;
@@ -47,14 +48,15 @@ in
         authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
       };
       postCommands = ''
-        ip link set dev eth0 up
+        ip addr
+        ip link set dev enp6s0 up
 
-        ip addr add ${cfg.ipv4.address}/${cfg.ipv4.cidr} dev eth0
-        ip route add ${cfg.ipv4.gateway} dev eth0
-        ip route add default via ${cfg.ipv4.gateway} dev eth0
+        ip addr add ${cfg.ipv4.address}/${cfg.ipv4.cidr} dev enp6s0
+        ip route add ${cfg.ipv4.gateway} dev enp6s0
+        ip route add default via ${cfg.ipv4.gateway} dev enp6s0
 
-        ip -6 addr add ${cfg.ipv6.address}/${cfg.ipv6.cidr} dev eth0
-        ip -6 route add default via fe80::1 dev eth0
+        ip -6 addr add ${cfg.ipv6.address}/${cfg.ipv6.cidr} dev enp6s0
+        ip -6 route add default via fe80::1 dev enp6s0
       '';
 
     };
