bf18f6b9bf9d7b3aea6f79e0b0c6645a66fb33f9
lassulus <git@lassul.us>
2024-02-12 07:27:55 +0100

    web01: point to current nixos-anywhere location

--- terraform/web01/main.tf
+++ terraform/web01/main.tf
@@ -2,7 +2,7 @@ locals {
 }
 
 module "deploy" {
-  source                 = "github.com/numtide/nixos-anywhere//terraform/all-in-one?ref=extra-files"
+  source                 = "github.com/nix-community/nixos-anywhere//terraform/all-in-one"
   nixos_system_attr      = ".#nixosConfigurations.web01.config.system.build.toplevel"
   nixos_partitioner_attr = ".#nixosConfigurations.web01.config.system.build.diskoScript"
   target_host            = var.ipv4_address
