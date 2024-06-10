1dc9adebf10808340377a9e784200dc99452f573
Jörg Thalheim <joerg@thalheim.io>
2024-06-10 12:10:20 +0200

    use unbound

--- modules/mailserver.nix
+++ modules/mailserver.nix
@@ -1,8 +1,4 @@
-{ config
-, pkgs
-, inputs
-, ...
-}:
+{ config, pkgs, ... }:
 let
   mailPassword =
     { service }:
@@ -26,12 +22,29 @@ in
     fqdn = "mail.clan.lol";
     domains = [ "clan.lol" ];
     enablePop3 = true;
+    # kresd sucks unfortunally (fails when one NS server is not working, instead of trying other ones)
+    localDnsResolver = false;
 
     loginAccounts."golem@clan.lol".hashedPasswordFile =
       config.clanCore.facts.services.golem-mail.secret.golem-password-hash.path;
     loginAccounts."gitea@clan.lol".hashedPasswordFile =
       config.clanCore.facts.services.gitea-mail.secret.gitea-password-hash.path;
   };
+
+  services.unbound = {
+    enable = true;
+    settings.server = {
+      prefetch = "yes";
+      prefetch-key = true;
+      qname-minimisation = true;
+      # Too many broken dnssec setups even at big companies such as amazon.
+      # Breaks my email setup. Better rely on tls for security.
+      val-permissive-mode = "yes";
+    };
+  };
+
+  # use local unbound as dns resolver
+  networking.nameservers = [ "127.0.0.1" ];
 
   security.acme.acceptTerms = true;
 
