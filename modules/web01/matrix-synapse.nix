2ca09ea5492f11d71e1790c16f15bf6dcf3a0b44
Qubasa <consulting@qube.email>
2024-07-02 13:42:59 +0200

    matrix: Increase login threshold for bots

--- modules/web01/matrix-synapse.nix
+++ modules/web01/matrix-synapse.nix
@@ -8,4 +8,23 @@
   };
   clan.matrix-synapse.users.monitoring = { };
   clan.matrix-synapse.users.clan-bot = { };
+
+  # Rate limiting settings
+  # we need to up this to be able to support matrix bots
+  services.matrix-synapse.settings = {
+    rc_login = {
+      address = {
+        per_second = 20;
+        burst_count = 200;
+      };
+      account = {
+        per_second = 20;
+        burst_count = 200;
+      };
+      failed_attempts = {
+        per_second = 3;
+        burst_count = 15;
+      };
+    };
+  };
 }
