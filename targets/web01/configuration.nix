eca556a5e4c76a80de819ab98713c03c6a2b6042
Jörg Thalheim <joerg@thalheim.io>
2024-07-09 11:55:19 +0200

    update clan-core

--- targets/web01/configuration.nix
+++ targets/web01/configuration.nix
@@ -7,7 +7,7 @@
   networking.hostName = "web01";
   systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f9:3080:418b::1";
 
-  clan.networking.targetHost = "root@clan.lol";
+  clan.core.networking.targetHost = "root@clan.lol";
   clan-infra.networking.ipv4.address = "65.21.12.51";
   clan-infra.networking.ipv4.gateway = "65.21.12.1";
   clan-infra.networking.ipv6.address =
