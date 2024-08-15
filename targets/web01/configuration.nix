bb3f4bcf5b0629fb45d099fe453b4e51acc15fa3
Jörg Thalheim <joerg@thalheim.io>
2024-08-15 16:46:54 +0200

    add new mail account

--- targets/web01/configuration.nix
+++ targets/web01/configuration.nix
@@ -7,6 +7,8 @@
   networking.hostName = "web01";
   systemd.network.networks."10-uplink".networkConfig.Address = "2a01:4f9:3080:418b::1";
 
+  clan.core.sops.defaultGroups = [ "admins" ];
+
   clan.core.networking.targetHost = "root@clan.lol";
   clan-infra.networking.ipv4.address = "65.21.12.51";
   clan-infra.networking.ipv4.gateway = "65.21.12.1";
