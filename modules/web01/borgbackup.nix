e3e15241036724e63cd90c24e06afa960fdce33f
Jörg Thalheim <joerg@thalheim.io>
2024-12-23 13:44:59 +0100

    update store box ssh keys
    bup clan flake

--- modules/web01/borgbackup.nix
+++ modules/web01/borgbackup.nix
@@ -1,10 +1,17 @@
 { config, self, ... }:
+let
+  user = "u366395";
+  host = "${user}.your-storagebox.de";
+in
+# Run this from the hetzner network
+# ssh-keyscan -p 23 <host>
+# and update known hosts section below
 {
   imports = [ self.inputs.clan-core.clanModules.borgbackup ];
 
   # 100GB storagebox is under the nix-community hetzner account
   clan.borgbackup.destinations.${config.networking.hostName} = {
-    repo = "u366395@u366395.your-storagebox.de:/./borgbackup";
+    repo = "${user}@${host}:/./borgbackup";
     rsh = "ssh -oPort=23 -i ${config.clan.core.facts.services.borgbackup.secret."borgbackup.ssh".path}";
   };
 
@@ -65,13 +72,14 @@
     "/var/log/telegraf"
   ];
 
-  # Run this from the hetzner network:
-  # ssh-keyscan -p 23 u359378.your-storagebox.de
   programs.ssh.knownHosts = {
-    storagebox-ecdsa.hostNames = [ "[u359378.your-storagebox.de]:23" ];
+    storagebox-ed25519.hostNames = [ "[${host}]:23" ];
+    storagebox-ed25519.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICf9svRenC/PLKIL9nk6K/pxQgoiFC41wTNvoIncOxs";
+
+    storagebox-ecdsa.hostNames = [ "[${host}]:23" ];
     storagebox-ecdsa.publicKey = "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGK0po6usux4Qv2d8zKZN1dDvbWjxKkGsx7XwFdSUCnF19Q8psHEUWR7C/LtSQ5crU/g+tQVRBtSgoUcE8T+FWp5wBxKvWG2X9gD+s9/4zRmDeSJR77W6gSA/+hpOZoSE+4KgNdnbYSNtbZH/dN74EG7GLb/gcIpbUUzPNXpfKl7mQitw==";
 
-    storagebox-rsa.hostNames = [ "[u359378.your-storagebox.de]:23" ];
+    storagebox-rsa.hostNames = [ "[${host}]:23" ];
     storagebox-rsa.publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5EB5p/5Hp3hGW1oHok+PIOH9Pbn7cnUiGmUEBrCVjnAw+HrKyN8bYVV0dIGllswYXwkG/+bgiBlE6IVIBAq+JwVWu1Sss3KarHY3OvFJUXZoZyRRg/Gc/+LRCE7lyKpwWQ70dbelGRyyJFH36eNv6ySXoUYtGkwlU5IVaHPApOxe4LHPZa/qhSRbPo2hwoh0orCtgejRebNtW5nlx00DNFgsvn8Svz2cIYLxsPVzKgUxs8Zxsxgn+Q/UvR7uq4AbAhyBMLxv7DjJ1pc7PJocuTno2Rw9uMZi1gkjbnmiOh6TTXIEWbnroyIhwc8555uto9melEUmWNQ+C+PwAK+MPw==";
   };
 }
