dc2f8e7507ec7f792c2499b9ae8dca38d2432488
Jörg Thalheim <joerg@thalheim.io>
2025-01-07 14:27:27 +0100

    add hetzner-cache

--- modules/web01/default.nix
+++ modules/web01/default.nix
@@ -14,6 +14,8 @@
     self.inputs.clan-core.clanModules.zt-tcp-relay
   ];
 
+  nix.settings.extra-substituters = [ "https://hetzner-cache.numtide.com" ];
+
   clan.sshd.hostKeys.rsa.enable = true;
   services.cloud-init.xfs.enable = true;
 }
