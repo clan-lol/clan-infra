9d31189463037c79618e81bb466f7b4d0557a99c
Michael Hoang <enzime@users.noreply.github.com>
2025-02-25 14:55:26 +0700

    terranix: fix `tofu state` subcommand

--- machines/flake-module.nix
+++ machines/flake-module.nix
@@ -95,6 +95,22 @@
             terraformWrapper.prefixText = ''
               TF_VAR_passphrase=$(clan secrets get tf-passphrase)
               export TF_VAR_passphrase
+              TF_ENCRYPTION=$(cat <<EOF
+              key_provider "pbkdf2" "state_encryption_password" {
+                passphrase = "$TF_VAR_passphrase"
+              }
+              method "aes_gcm" "encryption_method" {
+                keys = "\''${key_provider.pbkdf2.state_encryption_password}"
+              }
+              state {
+                enforced = true
+                method = "\''${method.aes_gcm.encryption_method}"
+              }
+              EOF
+              )
+
+              # shellcheck disable=SC2090
+              export TF_ENCRYPTION
             '';
           };
         };
