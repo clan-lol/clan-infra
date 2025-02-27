25f7c1f2047a1b1d85df8e9d5ccbe6900448b2e6
Jörg Thalheim <joerg@thalheim.io>
2025-02-27 18:47:16 +0700

    storinator01: set buildHost/targetHost

--- machines/storinator01/configuration.nix
+++ machines/storinator01/configuration.nix
@@ -13,5 +13,6 @@
 
   clan.core.sops.defaultGroups = [ "admins" ];
 
-  clan.core.networking.targetHost = "root@10.0.200.153";
+  clan.core.networking.targetHost = "root@[fda9:b487:2919:3547:3699:9393:7f57:6e6b]";
+  clan.core.networking.buildHost = "root@clan.lol";
 }
