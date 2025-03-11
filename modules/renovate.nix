7e4b2f35c4819b1294ff5bc193593ab7e56c641b
Jörg Thalheim <joerg@thalheim.io>
2025-03-11 17:13:12 +0100

    add renovate bot

---
+++ modules/renovate.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+{
+  services.renovate.enable = true;
+  services.renovate.runtimePackages = [
+    pkgs.git
+    pkgs.openssh
+    config.nix.package
+    # for Cargo.lock
+    pkgs.rustc
+    pkgs.cargo
+    # for go.mod
+    pkgs.go
+    pkgs.nodejs
+  ];
+  services.renovate.settings = {
+    labels = [
+      "dependencies"
+      "renovate"
+    ];
+    nix.enabled = true;
+    lockFileMaintenance.enabled = true;
+    automerge = true;
+    autodiscover = true;
+    autodiscoverTopics = [ "managed-by-renovate" ];
+    username = "renovate[bot]";
+    gitAuthor = "renovate[bot] <renovate@clan.lol>";
+    platform = "gitea";
+    endpoint = "https://git.clan.lol/api/v1/";
+    onboarding = true;
+    #allowedCommands = [
+    #  "^tslint --fix$"
+    #  "^tslint --[a-z]+$"
+    #];
+  };
+  services.renovate.credentials.RENOVATE_TOKEN =
+    config.clan.core.vars.generators.renovate-token.files.token.path;
+  services.renovate.credentials.GITHUB_COM_TOKEN =
+    config.clan.core.vars.generators.renovate-github.files.token.path;
+
+  clan.core.vars.generators.renovate-token = {
+    prompts.token.type = "hidden";
+    prompts.token.persist = true;
+    prompts.token.description = "Go to https://git.clan.lol/user/settings/applications and create a new token";
+  };
+
+  clan.core.vars.generators.renovate-github = {
+    prompts.token.type = "hidden";
+    prompts.token.persist = true;
+    prompts.token.description = "Go to https://git.clan.lol/user/settings/applications and create a new token";
+  };
+}
