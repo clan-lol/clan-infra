c062bcf3b8a7dc99c491a3843ef3b7cb780b9bd9
Michael Hoang <enzime@users.noreply.github.com>
2025-05-26 11:48:35 +1000

    renovate: switch to Mic92's latest branch

--- modules/renovate.nix
+++ modules/renovate.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
@@ -38,31 +37,6 @@
     #];
   };
   services.renovate.schedule = "*:0/10";
-  services.renovate.package = pkgs.renovate.overrideAttrs (
-    final: prev: {
-      # Remove `version`, `src` and `pnpmDeps` once we have a newer version in Nixpkgs
-      version =
-        assert lib.versionOlder prev.version "40.0.6";
-        "40.0.6+sandro";
-
-      # using it as a patch does not apply over any version that I know of...
-      # so let's just use Sandro's branch directly
-      src = pkgs.fetchFromGitHub {
-        owner = "SuperSandro2000";
-        repo = "renovate";
-        rev = "d3c715c0285f2d1186dcb2e889e0bda96d093cb6";
-        hash = "sha256-dviGWdVtBBD9PvXv5EJDy+s+wT/fcIhKYtO+mCzBD5o=";
-      };
-
-      # use `fetchDeps.override` when https://github.com/NixOS/nixpkgs/pull/407784 is merged
-      pnpmDeps =
-        assert !prev.pnpmDeps ? override;
-        pkgs.pnpm_10.fetchDeps {
-          inherit (final) pname version src;
-          hash = "sha256-v3coZiCgZm2eQDQTFtTdGqqUOXmjMIXuCHqJk1tdFys=";
-        };
-    }
-  );
   services.renovate.credentials.RENOVATE_TOKEN =
     config.clan.core.vars.generators.renovate-token.files.token.path;
   services.renovate.credentials.GITHUB_COM_TOKEN =
