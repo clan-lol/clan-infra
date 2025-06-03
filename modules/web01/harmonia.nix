9642ad5ff77517923e2c35ca1b406313e36a7630
DavHau <hsngrmpf+github@gmail.com>
2025-06-03 19:48:05 +0700

    cache-low-prio.clan.lol: init
    Adds another cache endpoint that advertises lower priority.
    
    This is needed for users who do not have a fast connection to our hetzner machine

--- modules/web01/harmonia.nix
+++ modules/web01/harmonia.nix
@@ -24,4 +24,27 @@
       zstd_types application/x-nix-archive;
     '';
   };
+
+  services.nginx.virtualHosts."cache-low-prio.clan.lol" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/".extraConfig = ''
+      proxy_pass http://127.0.0.1:5000;
+      proxy_set_header Host $host;
+      proxy_redirect http:// https://;
+      proxy_http_version 1.1;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection $connection_upgrade;
+
+      zstd on;
+      zstd_types application/x-nix-archive;
+
+      alias ${pkgs.writeText "cache-info" ''
+        StoreDir: /nix/store
+        WantMassQuery: 1
+        Priority: 42
+      ''};
+    '';
+  };
 }
