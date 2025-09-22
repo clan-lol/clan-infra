10d5f7d61b16f741cc708234bc51c19116bc4ac7
Jörg Thalheim <joerg@thalheim.io>
2025-09-22 13:41:14 +0200

    add an email address for myself and simplify adding users

--- modules/mailserver.nix
+++ modules/mailserver.nix
@@ -1,79 +1,93 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
-  mailPassword =
-    { service }:
-    {
-      files."${service}-password" = { };
-      files."${service}-password-hash" = { };
-      migrateFact = "${service}-mail";
-      runtimeInputs = with pkgs; [
-        coreutils
-        xkcdpass
-        mkpasswd
-      ];
-      script = ''
-        xkcdpass -n 4 -d - > $out/${service}-password
-        cat $out/${service}-password | mkpasswd -s -m bcrypt > $out/${service}-password-hash
-      '';
-    };
+  cfg = config.services.mailserver;
 in
 {
   imports = [
     ./acme.nix
   ];
 
-  mailserver = {
-    enable = true;
-    fqdn = "mail.clan.lol";
-    domains = [ "clan.lol" ];
-    enablePop3 = true;
-    enableImap = true;
-
-    certificateScheme = "acme-nginx";
-    # kresd sucks unfortunally (fails when one NS server is not working, instead of trying other ones)
-    localDnsResolver = false;
-
-    fullTextSearch.enable = true;
-
-    loginAccounts."golem@clan.lol".hashedPasswordFile =
-      config.clan.core.vars.generators.golem-mail.files.golem-password-hash.path;
-
-    loginAccounts."w@clan.lol".hashedPasswordFile =
-      config.clan.core.vars.generators.w-mail.files.w-password-hash.path;
-
-    loginAccounts."chris@clan.lol".hashedPasswordFile =
-      config.clan.core.vars.generators.chris-mail.files.chris-password-hash.path;
-
-    loginAccounts."gitea@clan.lol".hashedPasswordFile =
-      config.clan.core.vars.generators.gitea-mail.files.gitea-password-hash.path;
-
-    loginAccounts."kiran@clan.lol".hashedPasswordFile =
-      config.clan.core.vars.generators.kiran-mail.files.kiran-password-hash.path;
-
-    loginAccounts."timo@clan.lol".hashedPasswordFile =
-      config.clan.core.vars.generators.timo-mail.files.timo-password-hash.path;
+  options.services.mailserver.users = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [ ];
+    description = "List of usernames to create mail accounts for @clan.lol";
+    example = [
+      "alice"
+      "bob"
+    ];
   };
 
-  services.unbound = {
-    enable = true;
-    settings.server = {
-      prefetch = "yes";
-      prefetch-key = true;
-      qname-minimisation = true;
-      # Too many broken dnssec setups even at big companies such as amazon.
-      # Breaks my email setup. Better rely on tls for security.
-      val-permissive-mode = "yes";
+  config = {
+    services.mailserver.users = [
+      "golem"
+      "w"
+      "chris"
+      "gitea"
+      "kiran"
+      "timo"
+      "joerg"
+    ];
+
+    mailserver = {
+      enable = true;
+      fqdn = "mail.clan.lol";
+      domains = [ "clan.lol" ];
+      enablePop3 = true;
+      enableImap = true;
+
+      certificateScheme = "acme-nginx";
+      # kresd sucks unfortunally (fails when one NS server is not working, instead of trying other ones)
+      localDnsResolver = false;
+
+      fullTextSearch.enable = true;
+
+      loginAccounts = lib.listToAttrs (
+        map (user: {
+          name = "${user}@clan.lol";
+          value.hashedPasswordFile =
+            config.clan.core.vars.generators."${user}-mail".files."${user}-password-hash".path;
+        }) cfg.users
+      );
     };
+
+    services.unbound = {
+      enable = true;
+      settings.server = {
+        prefetch = "yes";
+        prefetch-key = true;
+        qname-minimisation = true;
+        # Too many broken dnssec setups even at big companies such as amazon.
+        # Breaks my email setup. Better rely on tls for security.
+        val-permissive-mode = "yes";
+      };
+    };
+
+    # use local unbound as dns resolver
+    networking.nameservers = [ "127.0.0.1" ];
+
+    clan.core.vars.generators = lib.listToAttrs (
+      map (user: {
+        name = "${user}-mail";
+        value = {
+          files."${user}-password" = { };
+          files."${user}-password-hash" = { };
+          migrateFact = "${user}-mail";
+          runtimeInputs = with pkgs; [
+            coreutils
+            xkcdpass
+            mkpasswd
+          ];
+          script = ''
+            xkcdpass -n 4 -d - > $out/${user}-password
+            cat $out/${user}-password | mkpasswd -s -m bcrypt > $out/${user}-password-hash
+          '';
+        };
+      }) cfg.users
+    );
   };
-
-  # use local unbound as dns resolver
-  networking.nameservers = [ "127.0.0.1" ];
-
-  clan.core.vars.generators.golem-mail = mailPassword { service = "golem"; };
-  clan.core.vars.generators.w-mail = mailPassword { service = "w"; };
-  clan.core.vars.generators.gitea-mail = mailPassword { service = "gitea"; };
-
-  clan.core.vars.generators.chris-mail = mailPassword { service = "chris"; };
-  clan.core.vars.generators.kiran-mail = mailPassword { service = "kiran"; };
-  clan.core.vars.generators.timo-mail = mailPassword { service = "timo"; };
 }
