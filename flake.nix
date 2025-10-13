c7cb4f49d051aa634a2799c2f887a8e52ede5034
Jörg Thalheim <joerg@thalheim.io>
2025-10-14 00:16:24 +0100

    faster niks3

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
-    niks3.url = "github:Mic92/niks3";
+    niks3.url = "github:Mic92/niks3/add-client";
     niks3.inputs.nixpkgs.follows = "nixpkgs";
     niks3.inputs.treefmt-nix.follows = "treefmt-nix";
     niks3.inputs.flake-parts.follows = "flake-parts";
