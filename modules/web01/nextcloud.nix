555f7efdfe9513ee4fb34952010df641dc5d8184
Michael Hoang <enzime@users.noreply.github.com>
2025-10-15 15:23:02 +0200

    nextcloud: configure OIDC with Gitea

--- modules/web01/nextcloud.nix
+++ modules/web01/nextcloud.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:
 let
@@ -18,6 +19,12 @@ in
     '';
   };
 
+  clan.core.vars.generators.nextcloud-oidc = {
+    prompts.client-id.persist = true;
+    files.client-id.secret = false;
+    files.client-id.deploy = false;
+  };
+
   services.nextcloud.enable = true;
   services.nextcloud.package = pkgs.nextcloud32;
   services.nextcloud.hostName = hostname;
@@ -27,6 +34,23 @@ in
     inherit (config.services.nextcloud.package.packages.apps) calendar user_oidc;
   };
   services.nextcloud.appstoreEnable = false;
+
+  # We don't need a client secret because we use PKCE
+  # however the `user_oidc:provider` command expects us
+  # to always pass a client secret.
+  systemd.services.nextcloud-setup.script = lib.mkAfter ''
+    ${lib.getExe config.services.nextcloud.occ} user_oidc:provider:delete Gitea --force
+    ${lib.getExe config.services.nextcloud.occ} user_oidc:provider Gitea \
+      --scope="openid email profile groups" \
+      --clientid=${lib.escapeShellArg config.clan.core.vars.generators.nextcloud-oidc.files.client-id.value} \
+      --clientsecret="" \
+      --discoveryuri="https://git.clan.lol/.well-known/openid-configuration" \
+      --group-provisioning=1 \
+      --group-whitelist-regex='/^clan:(owners|nextcloud)$/' \
+      --group-restrict-login-to-whitelist=1 \
+      --no-interaction
+  '';
+  services.nextcloud.settings.user_oidc.use_pkce = true;
 
   services.nextcloud.config = {
     dbtype = "pgsql";
