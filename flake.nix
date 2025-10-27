d21857cf392bef746001e2b28063637bfb78d633
Jörg Thalheim <joerg@thalheim.io>
2025-10-27 13:15:22 +0100

    add nix-community cachix key as well
    we had some paths signed with this key, that broke our ci.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   nixConfig = {
     extra-substituters = [ "https://cache.clan.lol" ];
-    extra-trusted-public-keys = [ "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28=" ];
+    extra-trusted-public-keys = [
+      "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   inputs = {
