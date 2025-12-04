89a5897c99b74be97359ea7123c66bdeafbffb38
Jörg Thalheim <joerg@thalheim.io>
2025-12-04 10:06:42 +0100

    import more dns entries into terraform

--- modules/terranix/dns.nix
+++ modules/terranix/dns.nix
@@ -66,6 +66,66 @@
       type = "A";
       value = "100.98.54.8";
     };
+
+    # Root domain
+    clan_lol_a = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "@";
+      type = "A";
+      value = "23.88.17.207";
+    };
+    clan_lol_aaaa = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "@";
+      type = "AAAA";
+      value = "2a01:4f8:2220:1565::1";
+    };
+
+    # Mail server
+    mail_clan_lol_a = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "mail";
+      type = "A";
+      value = "23.88.17.207";
+    };
+    mail_clan_lol_aaaa = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "mail";
+      type = "AAAA";
+      value = "2a01:4f8:2220:1565::1";
+    };
+
+    # MX record
+    clan_lol_mx = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "@";
+      type = "MX";
+      value = "10 mail.clan.lol.";
+    };
+
+    # SPF - only web01
+    clan_lol_spf = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "@";
+      type = "TXT";
+      value = "v=spf1 ip4:23.88.17.207 ip6:2a01:4f8:2220:1565::1 ~all";
+    };
+
+    # DMARC
+    clan_lol_dmarc = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "_dmarc";
+      type = "TXT";
+      value = "v=DMARC1; p=none; adkim=r; aspf=r; rua=mailto:joerg.dmarc@thalheim.io; ruf=mailto:joerg.dmarc@thalheim.io; pct=100";
+    };
+
+    # DKIM
+    clan_lol_dkim = {
+      zone_id = config.resource.hetznerdns_zone.clan_lol "id";
+      name = "mail._domainkey";
+      type = "TXT";
+      value = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdw2gyAg5TW2/OO2u8sbzlI6vfLkPycr4ufpfFQVvpd31hb6ctvpWXlzVHUDi9KyaWRydB7cAmYvPuZ7KFi1XPzQ213vy0S0AEbnXOJsTyT5FR8cmiuHPhiWGSMrSlB/l78kG6xK6A1x2lWCm2r7z/dzkLyCgAqI79YaUTcYO0eQIDAQAB";
+    };
   };
 
   output.clan_lol_zone_id = {
