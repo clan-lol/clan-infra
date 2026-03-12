36749b9e7708ab861e2573191c96d9093a7c85ac
Johannes Kirschbauer <hsjobeki@gmail.com>
2026-03-12 15:53:11 +0100

    web01/nginx: redirect docs.clan.lol to clan.lol/docs

--- modules/web01/homepage.nix
+++ modules/web01/homepage.nix
@@ -36,11 +36,6 @@
       locations."/".extraConfig = ''
         set $cors "false";
 
-        # Allow cross-origin requests from docs.clan.lol
-        if ($http_origin = "https://docs.clan.lol") {
-            set $cors "true";
-        }
-
         # Allow cross-origin requests from localhost IPs with port 8000
         if ($http_origin = "http://localhost:8000") {
             set $cors "true";
@@ -123,34 +118,20 @@
       '';
     };
 
-    # TODO: Once clan.lol/docs is verified working, redirect docs.clan.lol to clan.lol/docs
     virtualHosts."docs.clan.lol" = {
       forceSSL = true;
       enableACME = true;
-      # to be deployed via rsync
-      root = "/var/www/docs.clan.lol";
-      extraConfig = ''
-        charset utf-8;
-        source_charset utf-8;
-      '';
-
-      # Make sure to expire the cache after 12 hour
-      locations."/".extraConfig = ''
-        add_header Cache-Control "public, max-age=43200";
-        try_files $uri $uri.html $uri/ $uri/index.html =404;
-      '';
       locations."/blog/2024/03/19/introducing-clan-full-stack-computing-redefined/".return =
-        "307 https://clan.lol/blog/introduction-clan/";
+        "301 https://clan.lol/blog/introduction-clan/";
       locations."/blog/2024/05/25/jsonschema-converter/".return =
-        "307 https://clan.lol/blog/json-schema-converter/";
+        "301 https://clan.lol/blog/json-schema-converter/";
       locations."/blog/2024/06/24/backups/".return =
-        "307 https://clan.lol/blog/declarative-backups-and-restore/";
-      locations."/blog/2024/07/19/nixos-facter/".return = "307 https://clan.lol/blog/nixos-facter/";
-      locations."/blog/2024/09/11/interfaces/".return = "307 https://clan.lol/blog/interfaces/";
-
-      locations."^~ /blog".extraConfig = ''
-        rewrite ^/wclan(.*)$ https://clan.lol/blog permanent;
-      '';
+        "301 https://clan.lol/blog/declarative-backups-and-restore/";
+      locations."/blog/2024/07/19/nixos-facter/".return = "301 https://clan.lol/blog/nixos-facter/";
+      locations."/blog/2024/09/11/interfaces/".return = "301 https://clan.lol/blog/interfaces/";
+      locations."^~ /blog".return = "301 https://clan.lol/blog";
+      # Redirect docs.clan.lol -> clan.lol/docs
+      locations."/".return = "301 https://clan.lol/docs$request_uri";
     };
 
     virtualHosts."www.clan.lol" = {
