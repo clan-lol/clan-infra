b77c07040f76e891edba2e92f3f5b29a6e606ee8
Michael Hoang <enzime@users.noreply.github.com>
2026-05-10 15:36:00 +0200

    gitea: try not to block endpoints Nix uses

--- pkgs/gitea/default.nix
+++ pkgs/gitea/default.nix
@@ -4,6 +4,13 @@ gitea.overrideAttrs (old: {
   patches = old.patches ++ [
     ./0001-add-bot-check.patch
 
+    # https://github.com/Enzime/gitea/tree/push-ylslosqvwwts
+    (fetchpatch {
+      name = "try-not-to-block-anything-that-breaks-nix.patch";
+      url = "https://github.com/Enzime/gitea/commit/fda7b59413018c7ef4ac64d2f4dd1a896d1bee10.patch";
+      hash = "sha256-+G0nqTuY6M4p8vWnFY2UNNu4gx0VGrnrXELcqYsIStI=";
+    })
+
     (fetchpatch {
       name = "fix-automerge-accepting-disallowed-merge-styles.patch";
       url = "https://github.com/Enzime/gitea/commit/1df9c4582d7c9732e7f894cc07a7b70d4b6487f9.patch";
