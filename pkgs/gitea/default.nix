19fe2e47f9d023b9b97c98dfcea0aa3c73962ea9
Qubasa <consulting@qube.email>
2026-08-04 10:24:47 +0200

    gitea: Fix automerge-accepting-disallowed-merge patch

--- pkgs/gitea/default.nix
+++ pkgs/gitea/default.nix
@@ -4,11 +4,7 @@ gitea.overrideAttrs (old: {
   patches = old.patches ++ [
     ./0001-add-bot-check.patch
 
-    (fetchpatch {
-      name = "fix-automerge-accepting-disallowed-merge-styles.patch";
-      url = "https://github.com/Enzime/gitea/commit/1df9c4582d7c9732e7f894cc07a7b70d4b6487f9.patch";
-      hash = "sha256-Wmw4jCr8121Julp5vYL+owcwmopxCNbj/4IhGHQ6lSk=";
-    })
+    ./0002-fix-automerge-accepting-disallowed-merge-styles.patch
 
     (fetchpatch {
       name = "make-merge-style-optional.patch";
